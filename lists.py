7537e29e07db74b80a24051b329839f7730b94c3
Binybaby <36341077+Binybaby@users.noreply.github.com>
2018-03-19 10:25:05 +0000

    Update lists.py

--- lists.py
+++ lists.py
@@ -12,3 +12,13 @@ print("The element sqaures -1 is:",  squares[-1])
 print("The element sqaures -2 is:",  squares[-2])
 print("The element sqaures -3 is:",  squares[-3])
 print("The element sqaures -4 is:",  squares[-4])
+
+Words = ["cow", "eat", "grass"]
+print ("Words is:", Words)
+
+Mixed = [1, "cow", 3, 4]
+print ("Mixed is:", Mixed)
+
+print ("The length of squares is:", len (squares))
+print ("The length of Words is:", len (Words))
+print ("The length of Mixed is:", len (Mixed))
