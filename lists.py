5b998ee7ca9232742b1a08da54f32690536b4bbe
Binybaby <36341077+Binybaby@users.noreply.github.com>
2018-03-19 10:06:19 +0000

    Create lists.py

---
+++ lists.py
@@ -0,0 +1,14 @@
+# Biny Baby, 19-03-2018
+
+squares = [1, 4, 9, 16, 25]
+print(squares)
+
+print("The element sqaures 1 is:",  squares[1])
+print("The element sqaures 2 is:",  squares[2])
+print("The element sqaures 3 is:",  squares[3])
+print("The element sqaures 4 is:",  squares[4])
+
+print("The element sqaures -1 is:",  squares[-1])
+print("The element sqaures -2 is:",  squares[-2])
+print("The element sqaures -3 is:",  squares[-3])
+print("The element sqaures -4 is:",  squares[-4])
